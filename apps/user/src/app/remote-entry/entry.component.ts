31ab556893899a9bcc201b54bafecb55519fd705
Assaad Awada <a_awada78@hotmail.com>
2023-07-04 11:04:17 +0200

    Lab - Module Federation With Nx MF

---
+++ apps/user/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flight-workspace-user-entry',
+  template: `<flight-workspace-nx-welcome></flight-workspace-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
